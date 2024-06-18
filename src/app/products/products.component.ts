735090ecc3ccaa5cc5ccc264de4e5938d82c38f6
joyceenirmal123@gmail.com <joyceenirmal123@gmail.com>
2024-06-18 15:46:05 +0530

    done

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -10,7 +10,7 @@ export class ProductsComponent {
   products = [
     { imgSrc: 'assets/images/9.jpg', title: 'RK Cotton Seeds', rating: 5 },
     { imgSrc: 'assets/images/10.jpg', title: 'RK Groundnut Seeds', rating: 5 },
-    { imgSrc: 'assets/images/11.jpg', title: 'RK Maiza Seeds', rating: 5 },
+    { imgSrc: 'assets/images/11.jpg', title: 'RK Maize Seeds', rating: 5 },
     { imgSrc: 'assets/images/20.jpeg', title: 'RK Jowar Seeds', rating: 5 },
     { imgSrc: 'assets/images/21.jpeg', title: 'RK Green gram Seeds', rating: 5 },
     { imgSrc: 'assets/images/22.jpeg', title: 'RK Chilli Seeds', rating: 5 },
